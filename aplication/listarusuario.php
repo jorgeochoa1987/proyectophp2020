ba534ea0d75c4f7a951bf95eb6d7aca560af248c
jorge.ochoa <jorge.ochoa@nivelics.co-at-982536497151>
2020-12-02 22:16:25 -0500

    ajustes

--- aplication/listarusuario.php
+++ aplication/listarusuario.php
@@ -82,9 +82,7 @@ if(!isset($_SESSION['usuario']) and $_SESSION['estado'] != 'Autenticado') {
           <div class="main-navbar">
             <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
               <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
-                <div class="d-table m-auto">
-                  <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/menu.svg" alt="Menú de sistema">
-                </div>
+              
               </a>
               <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
                 <i class="material-icons">&#xE5C4;</i>
